023e2ec9a0051c6e1d24c67ca0b279ed2d24da5d
men <zhipeng.mzp@gmail.com>
2019-08-10 13:07:57 +0800

    同步

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -8,21 +8,54 @@ import 'package:dio/dio.dart';
 ///
 /// @author: mzp
 ///
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
+  @override
+  HomePageState createState() => new HomePageState();
+}
+
+class HomePageState extends State<HomePage> {
+  String test = "";
+
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
       appBar: new AppBar(
-        title: new Text('首页'),
+        title: new Text('百姓生活+'),
       ),
+      body: Center(child: Text(test)),
     );
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    queryData4Net();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+  }
+
+  @override
+  void didUpdateWidget(HomePage oldWidget) {
+    super.didUpdateWidget(oldWidget);
+  }
+
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
   }
 
   void queryData4Net() async {
     try {
       var response = await Dio().get(
-          "https://www.easy-mock.com/mock/5c60131a4bed3a6342711498/baixing/dabaojian");
-      print(response.data.toString());
+          "https://www.easy-mock.com/mock/5c60131a4bed3a6342711498/baixing/dabaojian",
+          queryParameters: {"name": "大大的高高的白白的"});
+
+      setState(() {
+        test = response.data.toString();
+      });
     } catch (e) {
       print(e);
     }
